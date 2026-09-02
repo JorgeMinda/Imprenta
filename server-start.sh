924d3dda2a20dbdd57824179d4b6cb2293870ac4
Jorge Leonardo Minda <jls.minda@yavirac.edu.ec>
2026-09-01 22:02:30 -0500

    fix(server): dynamically locate schema.prisma in server-start.sh

--- server-start.sh
+++ server-start.sh
@@ -3,5 +3,14 @@ set -e
 cd "$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
 cd .wasp/out/server
 npm install
-npx prisma migrate deploy --schema=../../schema.prisma
+if [ -f "../db/schema.prisma" ]; then
+  SCHEMA="../db/schema.prisma"
+elif [ -f "../../../schema.prisma" ]; then
+  SCHEMA="../../../schema.prisma"
+else
+  SCHEMA="schema.prisma"
+fi
+
+echo "Usando schema en: $SCHEMA"
+npx prisma migrate deploy --schema="$SCHEMA"
 npm run start-production
